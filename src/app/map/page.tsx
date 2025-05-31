de522afadb9441104396ca8876a3ad29c095a061
Alex Addams <alex.addams@coop.co.uk>
2025-05-31 11:40:30 +0100

    map renders

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Map from "@/components/Map";
 
 function Page() {
-  return <div>Page</div>;
+  return (
+    <div className="bg-white-700 mx-auto my-5 w-[98%] h-[480px]">
+      <Map posix={[4.79029, -75.69003]} />
+    </div>
+  );
 }
 
 export default Page;
